1f25c39b39c8aed87411cf4c403faa9eb94c4253
Carlos Ernesto Pineda Moreno <cpineda-@c1r6s1.42malaga.com>
2024-12-13 17:46:49 +0100

    memcpy in progress

--- Deadline 1/libft/ft_memcpy.c
+++ Deadline 1/libft/ft_memcpy.c
@@ -6,38 +6,39 @@
 /*   By: cpineda- <cpineda-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 19:14:11 by cpineda-          #+#    #+#             */
-/*   Updated: 2024/12/11 20:55:38 by cpineda-         ###   ########.fr       */
+/*   Updated: 2024/12/13 17:46:07 by cpineda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t	i;
+	size_t			i;
 	unsigned char	*str;
-	unsigned char	*strc;
+//	unsigned char	*strc;
 
 	i = 0;
 	str = (unsigned char *)src;
 	while ((str[i] != '\0') && (i < n))
 	{
-		strc[i] = str[i];
+		dest[i] = str[i];
 		i++;
 	}
-	src = strc;
+//	dest = strc;
+	return (dest);
 }
 
 int	main(void)
 {
 	char	s1[12] = "Hello World";
 	char	dest1[50];
-	ft_memcpy(0, s1, 5);
+	ft_memcpy('1', s1, 5);
 	printf ("%s\n", dest1);
 
 	char	s2[12] = "Hello World";
 	char	dest2[50];
-	ft_memcpy(0, s2, 5);
+	memcpy('1', s2, 5);
 	printf ("%s\n", dest2);
 	return (0);
 }
